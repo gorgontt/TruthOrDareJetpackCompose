9a2d8ed57acf203c5e0588e2e546025f92604203
gorgontt <166929134+gorgontt@users.noreply.github.com>
2025-02-17 18:14:06 +0300

    Revert "revert this"
    This reverts commit 570989681773ce8b7e1058eabdbdd250367e144c.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
     }
     buildFeatures {
         compose = true
-        viewBinding = true
     }
 }
 
@@ -50,11 +49,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.material)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
